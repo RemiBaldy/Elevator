27ae26b1e78db21f560e0d45503575ba46c60b77
m16001863 <gautier.maurice>
2019-10-07 13:33:56 +0200

    Controlleur a besoin d'être créé

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 
 
+import controller.BaseController;
+import controller.model.Model;
+import os.DummyOS;
 import view.DummyView;
 
 public class Main {
     public static void main(String[] args) {
 
+    	BaseController controller = new BaseController(new Model(5), new DummyOS());
         DummyView view = new DummyView(5);
     }
 
